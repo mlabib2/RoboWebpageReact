3d5cfde3d3da0050f858286ff776be648e5e3528
mlabib2 <mahirlive2000@gmail.com>
2024-01-01 20:30:07 +0800

    Updates

---
+++ precache-manifest.d585156ee3501f0f670c6f236190f305.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "568a75ed9e6383f35db2b93e0dfa0717",
+    "url": "/RoboWebpageReact/index.html"
+  },
+  {
+    "revision": "61c8e57c349c11809f58",
+    "url": "/RoboWebpageReact/static/css/2.a94b31b9.chunk.css"
+  },
+  {
+    "revision": "e2c067d7790f3cfbb251",
+    "url": "/RoboWebpageReact/static/css/main.d59ded1a.chunk.css"
+  },
+  {
+    "revision": "61c8e57c349c11809f58",
+    "url": "/RoboWebpageReact/static/js/2.e53ee963.chunk.js"
+  },
+  {
+    "revision": "9af3c6f5fec016cd9cb8",
+    "url": "/RoboWebpageReact/static/js/3.13bc5513.chunk.js"
+  },
+  {
+    "revision": "e2c067d7790f3cfbb251",
+    "url": "/RoboWebpageReact/static/js/main.d61b328c.chunk.js"
+  },
+  {
+    "revision": "e4594a3c1dc7cc9ad796",
+    "url": "/RoboWebpageReact/static/js/runtime~main.6ca6cea2.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/RoboWebpageReact/static/media/SEGA.82e09f78.TTF"
+  }
+]);
